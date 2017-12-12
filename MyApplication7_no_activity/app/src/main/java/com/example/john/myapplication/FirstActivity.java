39ddbaaf8520d46324ca8591c5c8fa76a3b6f47d
fujianlijiahan <1138416676@qq.com>
2017-12-12 16:04:38 +0800

    添加一个安卓项目

---
+++ MyApplication7_no_activity/app/src/main/java/com/example/john/myapplication/FirstActivity.java
@@ -0,0 +1,12 @@
+package com.example.john.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FirstActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
