81aa182e1942c87ab64ca3a877c05d3920e59256
fujianlijiahan <1138416676@qq.com>
2017-12-13 09:05:26 +0800

    稍作更改

--- MyApplication7_no_activity/app/src/main/java/com/example/john/myapplication/SixActivity.java
+++ MyApplication7_no_activity/app/src/main/java/com/example/john/myapplication/SixActivity.java
@@ -2,12 +2,22 @@ package com.example.john.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.graphics.drawable.Drawable;
+import android.widget.TextView;
+import android.util.Log;
 
 public class SixActivity extends AppCompatActivity {
-
+    private TextView txtZQD;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.text_view);
+        setContentView(R.layout.text_view3);
+        txtZQD = (TextView) findViewById(R.id.txtZQD);
+
+        Drawable[] drawable = txtZQD.getCompoundDrawables();
+        // 数组下表0~3,依次是:左上右下
+        drawable[1].setBounds(100, 0, 200, 200);
+        txtZQD.setCompoundDrawables(drawable[0], drawable[1], drawable[2],
+                drawable[3]);
     }
 }
