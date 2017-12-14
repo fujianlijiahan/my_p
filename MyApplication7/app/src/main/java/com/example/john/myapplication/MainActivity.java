bee9dff131e141f66ac9b089820cc1c4ed7ff07b
fujianlijiahan <1138416676@qq.com>
2017-12-14 17:11:48 +0800

    upp

--- MyApplication7/app/src/main/java/com/example/john/myapplication/MainActivity.java
+++ MyApplication7/app/src/main/java/com/example/john/myapplication/MainActivity.java
@@ -7,30 +7,30 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    private Button btnOne,btnTwo;
+    //private Button btnOne,btnTwo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.button);
-        btnOne = (Button) findViewById(R.id.btnOne);
-
-        btnTwo = (Button) findViewById(R.id.btnTwo);
-        Log.v("btnOne", btnOne.getText().toString());
-        Log.v("btnTwo", btnTwo.getText().toString());
-        btnTwo.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(btnTwo.getText().toString().equals("按钮不可用")){
-                    Log.v("按钮是否可用","打印出按钮可用");
-                    btnOne.setEnabled(false);
-                    btnTwo.setText("按钮可用");
-                }else{
-                    Log.v("按钮是否可用","打印出按钮不不不不不不不不可用");
-                    btnOne.setEnabled(true);
-                    btnTwo.setText("按钮不可用");
-                }
-            }
-        });
+        setContentView(R.layout.checkbox);
+//        btnOne = (Button) findViewById(R.id.btnOne);
+//
+//        btnTwo = (Button) findViewById(R.id.btnTwo);
+//        Log.v("btnOne", btnOne.getText().toString());
+//        Log.v("btnTwo", btnTwo.getText().toString());
+//        btnTwo.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                if(btnTwo.getText().toString().equals("按钮不可用")){
+//                    Log.v("按钮是否可用","打印出按钮可用");
+//                    btnOne.setEnabled(false);
+//                    btnTwo.setText("按钮可用");
+//                }else{
+//                    Log.v("按钮是否可用","打印出按钮不不不不不不不不可用");
+//                    btnOne.setEnabled(true);
+//                    btnTwo.setText("按钮不可用");
+//                }
+//            }
+//        });
 
     }
 }
